aef9e06d74e30b23f3dcb08267c00245fbb91f9f
julia-ms <mombachdasilva@gmail.com>
2023-12-25 23:23:28 -0300

    first tests

--- ImbCoL/test.R
+++ ImbCoL/test.R
@@ -17,6 +17,12 @@ ImbCoL::overlapping(Species ~ ., iris)
 install.packages('RWeka')
 library(RWeka)
 
-# dataset for tests
+# datasets for tests
 dataset = read.arff('Australian.arff')
+#dataset = read.arff('CastMetal1.arff')
+#dataset = read.arff('CostaMadre1.arff')
+#dataset = read.arff('KungChi3.arff')
+#dataset = read.arff('KnuggetChase3.arff')
+
+ImbCoL::complexity(class ~ ., dataset)
 
